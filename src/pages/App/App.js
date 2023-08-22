198cc7a9e6243b847b7245daa16724947c9ecf08
Joel Marcos Graf <55204995+Joel-Graf@users.noreply.github.com>
2023-08-22 11:36:09 -0300

    Rodando o teste

---
+++ src/pages/App/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import "./reset.css";
+import Comp from "../../components/Comp";
+
+function App() {
+  return (
+    <div className="App">
+      <Comp></Comp>
+    </div>
+  );
+}
+
+export default App;
