8454fdf98725e88a13b1981a71715ac7cc1dd7c6
Joel Marcos Graf <55204995+Joel-Graf@users.noreply.github.com>
2023-09-05 15:18:32 -0300

    up

--- src/pages/Exercise/Exercise.js
+++ src/pages/Exercise/Exercise.js
@@ -1,10 +1,23 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import Question from "./components/Question/Question";
+import Simulation from "./components/Simulation/Simulation";
 
 const Exercise = ({ exercise }) => {
+  // TODO:
+  function onSubmitAnswer(submitedAnswer) {
+    console.log("submitedAnswer: ", submitedAnswer);
+  }
+
   return (
-    <Container>
-      <h1>{exercise.name}</h1>
+    <Container
+      style={{
+        height: "100 vh",
+        width: "100 vw",
+      }}
+    >
+      <Question exercise={exercise} onSubmitAnswer={onSubmitAnswer} />
+      <Simulation exercise={exercise} />
     </Container>
   );
 };
