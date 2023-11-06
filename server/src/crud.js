9286aa507f182b287cf4f9aec071076d7c0530cb
Gustavo Blasius <gustavo.blasius@clinicorp.com>
2023-11-05 22:46:53 -0300

    feat: initial crud structure

--- server/src/crud.js
+++ server/src/crud.js
@@ -1,26 +1,76 @@
 var admin = require("firebase-admin");
-var serviceAccount = require("./serviceAccount.json");
+var serviceAccount = require("../serviceAccount.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
-const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
-const db = getFirestore();
+const { getFirestore } = require('firebase-admin/firestore');
+const { collection, getDocs, doc, addDoc, setDoc, deleteDoc } = require('firebase/firestore');
+const db = getFirestore(); 
+class Crud{ //! Testar
+    email;
 
+    constructor(email) {
+        this.email = email;
+    }
 
-async function teste(){
-  const data = {
-    name: 'Los Angeles',
-    state: 'CA',
-    country: 'USA'
-  };
-  
-  // Add a new document in collection "cities" with ID 'LA'
-  const res = await db.collection('cities').doc('PA').set(data);
-  return res
+    async update(userSaveData, data, id){
+        try {
+            const {userEmail, kind} = userSaveData
+            const _post = doc(db, `psicodevlicos`, userEmail, kind, id)
+            return setDoc(_post, data)
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async create(userSaveData, data) {
+        try {
+            const {userEmail, kind} = userSaveData;
+            const _post = collection(db, `psicodevlicos/${userEmail}/${kind}`)
+            const resp = await addDoc(_post, data);
+            return { ...data, id: resp.id }   
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async remove(userSaveData, id){
+        try {
+            const {userEmail, kind} = userSaveData;
+            const removeItem = doc(db, `psicodevlicos`, userEmail, kind, id)
+            const resp = await deleteDoc(removeItem)
+            return resp;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async listAll(userSaveData){
+        try {
+            const {userEmail, kind} = userSaveData;
+            const postCol = collection(db, `psicodevlicos/${userEmail}/${kind}`);
+            const resp = await getDocs(postCol);
+            console.log("resp", resp)
+            const returnList = resp.docs.map((doc) => { return { id: doc.id, ...doc.data()}});
+            return returnList;
+        } catch (error) {
+            console.log('error', error)
+            throw error;
+        }
+    }
+
+    async getById(entity){
+        try {
+            const citiesCol = collection(db, entity);
+            const resp = await getDocs(citiesCol);
+            const returnList = resp.docs.map((doc) => doc.data());
+            return returnList;
+        } catch (error) {
+            throw error;
+        }
+    }
 }
 
-teste().then(res=>{
-  console.log('res', res)
-})
+module.exports.Crud = Crud;
