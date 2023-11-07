c5c9b398b6dccf32e0ab0dd7be9791578f84d541
Joel Marcos Graf <55204995+Joel-Graf@users.noreply.github.com>
2023-11-07 01:21:15 -0300

    Update Simulation2v2.js

--- client/src/pages/Exercise/components/Simulation/Simulations/Simulation2v2.js
+++ client/src/pages/Exercise/components/Simulation/Simulations/Simulation2v2.js
@@ -48,6 +48,12 @@ const Simulation2 = ({ speed, reset }) => {
     });
     Composite.add(engine.world, ground);
 
+    const teto = Bodies.rectangle(cw / 2, 0, cw, 120, {
+      isStatic: true,
+      friction: 0.8,
+    });
+    Composite.add(engine.world, teto);
+
     const mouse = Mouse.create(render.canvas);
     const mouseConstraint = MouseConstraint.create(engine, {
       mouse: mouse,
@@ -61,53 +67,108 @@ const Simulation2 = ({ speed, reset }) => {
     Composite.add(engine.world, mouseConstraint);
     render.mouse = mouse;
 
-    const angleInRadiansA = (150 * Math.PI) / 180; // Converter 45 graus para radianos
-    const ropeLengthA = 280; // Distância vertical entre o ponto de ancoragem e o centro de massa
+    const angleInRadiansA = (150 * Math.PI) / 180; // Convert 45 degrees to radians
+    const ropeLengthA = 260; // Vertical distance between the anchor point and the center of mass
 
     const ballXA = 300 + ropeLengthA * Math.cos(angleInRadiansA);
-    const ballYA = 70 + ropeLengthA * Math.sin(angleInRadiansA);
+    const ballYA = 120 + ropeLengthA * Math.sin(angleInRadiansA);
 
-    const ballA = Bodies.circle(ballXA, ballYA, 30, {
+    const ballA = Bodies.circle(ballXA, ballYA, 25, {
       density: 0.04,
       frictionAir: 0.005,
     });
     const ballConstraintA = Constraint.create({
-      pointA: { x: 300, y: 70 },
+      pointA: { x: 300, y: 120 },
       bodyB: ballA,
     });
+
+    // Add labels to the balls during creation
+    ballA.label = "250g - 26cm";
+
+    // Create HTML elements for the labels
+    const labelBallA = document.createElement("div");
+    labelBallA.innerText = ballA.label;
+    labelBallA.style.position = "absolute";
+    labelBallA.style.left = `${ballA.position.x - 25}px`;
+    labelBallA.style.top = `${ballA.position.y - 70}px`;
+    labelBallA.style.color = "red";
+    labelBallA.style.font = "16px Arial";
+    scene.current.appendChild(labelBallA);
+
     Composite.add(engine.world, [ballA, ballConstraintA]);
 
-    const angleInRadiansB = (150 * Math.PI) / 180; // Converter 45 graus para radianos
-    const ropeLengthB = 140; // Distância vertical entre o ponto de ancoragem e o centro de massa
+    const angleInRadiansB = (150 * Math.PI) / 180; // Convert 45 degrees to radians
+    const ropeLengthB = 130; // Vertical distance between the anchor point and the center of mass
 
     const ballXB = 300 + ropeLengthB * Math.cos(angleInRadiansB);
-    const ballYB = 70 + ropeLengthB * Math.sin(angleInRadiansB);
+    const ballYB = 120 + ropeLengthB * Math.sin(angleInRadiansB);
 
-    const ballB = Bodies.circle(ballXB, ballYB, 30, {
+    const ballB = Bodies.circle(ballXB, ballYB, 25, {
       density: 0.04,
       frictionAir: 0.005,
     });
     const ballConstraintB = Constraint.create({
-      pointA: { x: 300, y: 70 },
+      pointA: { x: 300, y: 120 },
       bodyB: ballB,
     });
+
+    // Create and append the label for ballB
+    ballB.label = "250g - 13cm"; // Adjust the label as needed
+
+    const labelBallB = document.createElement("div");
+    labelBallB.innerText = ballB.label;
+    labelBallB.style.position = "absolute";
+    labelBallB.style.left = `${ballB.position.x - 25}px`;
+    labelBallB.style.top = `${ballB.position.y - 70}px`;
+    labelBallB.style.color = "blue"; // You can change the color
+    labelBallB.style.font = "16px Arial";
+    scene.current.appendChild(labelBallB);
+
     Composite.add(engine.world, [ballB, ballConstraintB]);
 
-    const angleInRadiansC = (150 * Math.PI) / 180; // Converter 45 graus para radianos
-    const ropeLengthC = 280; // Distância vertical entre o ponto de ancoragem e o centro de massa
+    const angleInRadiansC = (150 * Math.PI) / 180; // Convert 45 degrees to radians
+    const ropeLengthC = 260; // Vertical distance between the anchor point and the center of mass
 
     const ballXC = 950 + ropeLengthC * Math.cos(angleInRadiansC);
-    const ballYC = 70 + ropeLengthC * Math.sin(angleInRadiansC);
+    const ballYC = 120 + ropeLengthC * Math.sin(angleInRadiansC);
 
-    const ballC = Bodies.circle(ballXC, ballYC, 45, {
+    const ballC = Bodies.circle(ballXC, ballYC, 35, {
       density: 0.08,
       frictionAir: 0.005,
     });
     const ballConstraintC = Constraint.create({
-      pointA: { x: 950, y: 70 },
+      pointA: { x: 950, y: 120 },
       bodyB: ballC,
     });
+
+    // Create and append the label for ballC
+    ballC.label = "350g - 26cm"; // Adjust the label as needed
+
+    const labelBallC = document.createElement("div");
+    labelBallC.innerText = ballC.label;
+    labelBallC.style.position = "absolute";
+    labelBallC.style.left = `${ballC.position.x - 25}px`;
+    labelBallC.style.top = `${ballC.position.y - 70}px`;
+    labelBallC.style.color = "green"; // You can change the color
+    labelBallC.style.font = "16px Arial";
+    scene.current.appendChild(labelBallC);
+
     Composite.add(engine.world, [ballC, ballConstraintC]);
+
+    const updateLabelPositions = () => {
+      labelBallA.style.left = `${ballA.position.x - 40}px`;
+      labelBallA.style.top = `${ballA.position.y - 55}px`;
+
+      labelBallB.style.left = `${ballB.position.x - 40}px`;
+      labelBallB.style.top = `${ballB.position.y - 55}px`;
+
+      labelBallC.style.left = `${ballC.position.x - 40}px`;
+      labelBallC.style.top = `${ballC.position.y - 70}px`;
+
+      requestAnimationFrame(updateLabelPositions);
+    };
+
+    updateLabelPositions();
 
     return () => {
       World.clear(engine.world);
@@ -118,6 +179,15 @@ const Simulation2 = ({ speed, reset }) => {
       render.canvas = null;
       render.context = null;
       render.textures = {};
+      if (scene?.current?.contains(labelBallA)) {
+        scene.current.removeChild(labelBallA);
+      }
+      if (scene?.current?.contains(labelBallB)) {
+        scene.current.removeChild(labelBallB);
+      }
+      if (scene?.current?.contains(labelBallC)) {
+        scene.current.removeChild(labelBallC);
+      }
     };
   }, [reset]);
 
